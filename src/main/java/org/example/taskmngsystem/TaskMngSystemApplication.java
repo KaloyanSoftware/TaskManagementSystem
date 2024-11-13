54368041aaf565309135c81f0ccfbc7e185270e4
Kaloyan Ivanov <kaloyan.ivanov@student.kdg.be>
2024-11-13 19:40:18 +0100

    first commit

---
+++ src/main/java/org/example/taskmngsystem/TaskMngSystemApplication.java
@@ -0,0 +1,12 @@
+package org.example.taskmngsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMngSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMngSystemApplication.class, args);
+    }
+}
---
+++ src/test/java/org/example/taskmngsystem/TaskMngSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.taskmngsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMngSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
